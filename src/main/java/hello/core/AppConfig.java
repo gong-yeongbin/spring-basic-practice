013efd13aa75fa1726dd7d5cd7d1c4a855ec30f9
gong-yeongbin <youngbin816@gmail.com>
2026-04-12 20:00:09 +0900

    feat: XML 기반의 애플리케이션 컨텍스트 설정 추가
    - appConfig.xml 파일을 추가하여 XML 설정을 통한 빈 정의를 구성함
    - BeanDefinitinoTest.java 파일을 추가하여 빈 설정 메타정보를 확인하는 테스트 케이스 작성
    - XmlAppContext.java 파일을 추가하여 XML 설정을 통해 MemberService 빈을 가져오는 테스트 케이스 작성
    - AppConfig.java에서 주석 처리된 FixDiscountPolicy 관련 코드 수정

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -31,7 +31,7 @@ public class AppConfig {
 
     @Bean
     public DiscountPolicy discountPolicy(){
-//        return new FixDiscountPolicy();
+//        ret   urn new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
 
